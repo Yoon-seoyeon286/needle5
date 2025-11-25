ba4021bfef221eaec097d3f784b829e6e7cd62fc
Hydra <panopticon0847@gmail.com>
2025-11-25 16:12:57 +0900

    featLsdad

---
+++ src/generated/gen.js
@@ -0,0 +1,17 @@
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+import url_0 from "/assets/SampleScene.glb?url";
+needle_exported_files.push(url_0);
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1764054699361");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 4\.11\.5");
