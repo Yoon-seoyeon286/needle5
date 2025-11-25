4579820f05c95851daeaa2ef4b18ea585350b8d2
Hydra <panopticon0847@gmail.com>
2025-11-25 16:31:48 +0900

    Updates

--- needle-app.js
+++ needle-app.js
@@ -53,7 +53,7 @@ if (!customElements.get(componentName)) {
             while(this.basePath.endsWith('/')) {
                 this.basePath = this.basePath.slice(0, -1);
             }
-            script.src = this.getAttribute('script-src') || `${this.basePath}/assets/index-efd4e23c.js`;
+            script.src = this.getAttribute('script-src') || `${this.basePath}/assets/index-9de7cb6f.js`;
             this.shadowRoot.appendChild(script);
 
             this.needleEngine = document.createElement('needle-engine');
